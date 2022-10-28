5775a022f8a186bda030415cbba1c4ffe509af2b
Amy Kapernick <amy@kapers.dev>
2022-10-28 10:42:47 +0800

    initial version of action to test it out

---
+++ README.md
@@ -0,0 +1,3 @@
+# A11y Testing GitHub Actions
+
+This action runs various a11y tests on the provided URL as a GitHub action.
---
+++ index.js
@@ -0,0 +1,20 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+	// `url` input defined in action metadata file
+	const url = core.getInput('url');
+	console.log(`Running tests on ${url}`);
+
+	const errors = 0
+
+	core.setOutput("errors", errors);
+	core.setOutput("status", "pass");
+
+	// Get the JSON webhook payload for the event that triggered the workflow
+	const payload = JSON.stringify(github.context.payload, undefined, 2)
+	console.log(`The event payload: ${payload}`);
+
+} catch (error) {
+	core.setFailed(error.message);
+}
